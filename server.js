8e5ee59c8ad269df27f7007e747b35b6cb4d2348
saif121A <156455502+saif121A@users.noreply.github.com>
2025-08-17 21:35:25 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-let products = [];
+let products = []; // مؤقت، لاحقًا ممكن MySQL
 
 app.get('/products', (req, res) => {
   res.json(products);
@@ -17,5 +16,5 @@ app.post('/products', (req, res) => {
   res.json(item);
 });
 
-const port = 3000;
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
